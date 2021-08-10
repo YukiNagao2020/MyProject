e05f20d5c20a7b959c75aa99335ed87303c66ba4
YukiNagao2020 <yuki.n0301@gmail.com>
2021-08-10 04:02:39 +0000

    Add category detail pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,10 +33,85 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+ 
+  def category1
+    @category = Category.find(1)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end  
+  
+  def category2
+    @category = Category.find(2)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end 
+  
+  def category3
+    @category = Category.find(3)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  
+  def category4
+    @category = Category.find(4)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  
+  def category5
+    @category = Category.find(5)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category6
+    @category = Category.find(6)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category7
+    @category = Category.find(7)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category8
+    @category = Category.find(8)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category9
+    @category = Category.find(9)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category10
+    @category = Category.find(10)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category11
+    @category = Category.find(11)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category12
+    @category = Category.find(12)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category13
+    @category = Category.find(13)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category14
+    @category = Category.find(14)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category15
+    @category = Category.find(15)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category16
+    @category = Category.find(16)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  def category17
+    @category = Category.find(17)
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
+  end
+  
   def category
     @category = Category.find(params[:id])
     @post = Post.includes(:categories).where(post_category_relations: { category_id: @category }).page(params[:page]).reverse_order.order(created_at: :desc)
   end
+  
    private
 
   def post_params
